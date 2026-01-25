a3ce5140e8a32f6db633a88b66e0d6acb1401aa8
pubudubandara <pubudumb@gmail.com>
2026-01-25 22:32:37 +0530

    feat: add authentication REST endpoints

---
+++ backend/src/main/java/com/finance/tracker/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.finance.tracker.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor //To generate constructor with required arguments (final fields)
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
