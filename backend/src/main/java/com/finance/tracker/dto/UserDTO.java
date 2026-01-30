b6bcf5630d27ffba17dd993d296762820384dac3
pubudubandara <pubudumb@gmail.com>
2026-01-30 14:18:23 +0530

    Add UserDTO for user data transfer

---
+++ backend/src/main/java/com/finance/tracker/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.finance.tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+}
